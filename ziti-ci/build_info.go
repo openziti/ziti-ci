375aea41901286cf0bca555769c402badb90c9b6
Travis CI User <travis@example.org>
2020-02-12 18:53:12 +0000

    Release v0.4.2

--- ziti-ci/build_info.go
+++ ziti-ci/build_info.go
@@ -3,9 +3,9 @@
 package main
 
 const (
-	Version   = "v0.3.11"
-	Revision  = "169c17d6f16f"
+	Version   = "v0.4.2"
+	Revision  = "c2aca0629057"
 	Branch    = "v1"
-	BuildUser = "Paul Lorenz"
-	BuildDate = "2020-02-10 14:50:41"
+	BuildUser = "Travis CI User"
+	BuildDate = "2020-02-12 18:53:12"
 )
