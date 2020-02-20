33f925d9cda0dbefa6bc7d671d9a081a0b0b1da1
ziti-ci <ziti-ci@netfoundry.io>
2020-02-20 21:15:18 +0000

    Release v0.5.10

--- ziti-ci/build_info.go
+++ ziti-ci/build_info.go
@@ -3,9 +3,9 @@
 package main
 
 const (
-	Version   = "v0.3.11"
-	Revision  = "169c17d6f16f"
+	Version   = "v0.5.10"
+	Revision  = "6e8c60bf6a4c"
 	Branch    = "v1"
-	BuildUser = "Paul Lorenz"
-	BuildDate = "2020-02-10 14:50:41"
+	BuildUser = "Travis CI User"
+	BuildDate = "2020-02-20 21:15:18"
 )
