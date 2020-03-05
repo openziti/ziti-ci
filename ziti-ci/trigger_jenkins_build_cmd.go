49205561e389da9505f6ba0316d2ad4b85156a6d
Paul Lorenz <paul.lorenz@netfoundry.io>
2020-03-05 13:02:46 -0500

    Use non-build number version when publishing master build

--- ziti-ci/trigger_jenkins_build_cmd.go
+++ ziti-ci/trigger_jenkins_build_cmd.go
@@ -44,12 +44,16 @@ func (cmd *triggerJenkinsSmokeBuildCmd) execute() {
 
 	client := resty.New()
 
-	targetVersion := fmt.Sprintf("%v-%v", cmd.getPublishVersion(), cmd.getBuildNumber())
+	version := cmd.getPublishVersion().String()
+	if cmd.getCurrentBranch() != "master" {
+		version = fmt.Sprintf("%v-%v", version, cmd.getBuildNumber())
+	}
+
 	resp, err := client.R().
 		EnableTrace().
 		SetQueryParam("token", cmd.jenkinsJobToken).
 		SetQueryParam("branch", cmd.getCurrentBranch()).
-		SetQueryParam("version", targetVersion).
+		SetQueryParam("version", version).
 		SetQueryParam("committer", cmd.getCommitterEmail()).
 		SetQueryParam("cause", fmt.Sprintf("triggered by Travis ziti-cmd build #%v", cmd.getBuildNumber())).
 		SetBasicAuth(cmd.jenkinsUser, cmd.jenkinsUserToken).
