9a36734a2b76584bd1bc3f65546a5f2b01547845
ziti-ci <ziti-ci@netfoundry.io>
2020-05-15 16:54:42 -0400

    add copyright and change os.exit to use 1 not -1

--- ziti-ci/common.go
+++ ziti-ci/common.go
@@ -1,3 +1,20 @@
+/*
+ * Copyright NetFoundry, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 package main
 
 import (
@@ -48,7 +65,7 @@ type baseCommand struct {
 
 func (cmd *baseCommand) failf(format string, params ...interface{}) {
 	_, _ = fmt.Fprintf(cmd.cmd.ErrOrStderr(), format, params...)
-	os.Exit(-1)
+	os.Exit(1)
 }
 
 func (cmd *baseCommand) infof(format string, params ...interface{}) {
