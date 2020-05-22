5eb0cbdf56638d891032a420068d530b40cac405
ziti-ci <ziti-ci@netfoundry.io>
2020-05-22 12:12:37 -0400

    added one too many spaces... _sigh_

--- ziti-ci/build_info.go
+++ ziti-ci/build_info.go
@@ -3,9 +3,9 @@
 package main
 
 const (
-       Version   = "v0.3.11"
-       Revision  = "169c17d6f16f"
-       Branch    = "v1"
-       BuildUser = "Paul Lorenz"
-       BuildDate = "2020-02-10 14:50:41"
+      Version   = "v0.3.11"
+      Revision  = "169c17d6f16f"
+      Branch    = "v1"
+      BuildUser = "Paul Lorenz"
+      BuildDate = "2020-02-10 14:50:41"
 )
