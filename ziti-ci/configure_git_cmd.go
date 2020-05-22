adf242d4af60d8d39531ceecd2d248b9af978ae5
dovholuknf <46322585+dovholuknf@users.noreply.github.com>
2020-05-22 11:54:51 -0400

    first try to only add .gitignore if it's not there yet

--- ziti-ci/configure_git_cmd.go
+++ ziti-ci/configure_git_cmd.go
@@ -18,11 +18,14 @@
 package main
 
 import (
+	"bufio"
 	"encoding/base64"
 	"fmt"
 	"github.com/spf13/cobra"
 	"io/ioutil"
 	"os"
+	"path/filepath"
+	"strings"
 )
 
 const (
@@ -55,15 +58,43 @@ func (cmd *configureGitCmd) execute() {
 		cmd.failf("unable to read ssh key from env var %v. Found? %v\n", cmd.sshKeyEnv, found)
 	}
 
-	//add the deploy key to .gitignore...
-	f, err := os.OpenFile(".gitignore",
-		os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	keyDir, err := filepath.Abs(cmd.sshKeyFile)
 	if err != nil {
-		cmd.failf("could not write to .gitignore", err)
+		cmd.failf("unable to read path for sshKeyFile? %v\n", cmd.sshKeyFile)
 	}
-	defer f.Close()
-	if _, err := f.WriteString(DefaultSshKeyFile + "\n"); err != nil {
-		cmd.failf("error writing to .gitignore", err)
+
+	ignoreExists := false
+
+	file, err := os.Open(keyDir + "/.gitignore")
+	if err != nil {
+		//probably means the file isn't there etc. just ignore this particular error
+	} else {
+		scanner := bufio.NewScanner(file)
+		for scanner.Scan() {
+			if strings.Contains(scanner.Text(), cmd.sshKeyFile) {
+				cmd.infof(".gitignore file already contains entry for " + cmd.sshKeyFile)
+				ignoreExists = true
+			}
+		}
+
+		if !ignoreExists {
+			//add the deploy key to .gitignore... next to whereever the sshkey goes...
+			f, err := os.OpenFile(keyDir + "/.gitignore",
+				os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+			if err != nil {
+				cmd.failf("could not write to .gitignore", err)
+			}
+			defer f.Close()
+			if _, err := f.WriteString(cmd.sshKeyFile + "\n"); err != nil {
+				cmd.failf("error writing to .gitignore", err)
+			}
+		}
+
+		if err := scanner.Err(); err != nil {
+			//probably means the file isn't there etc. just ignore this particular error
+		}
+
+		file.Close()
 	}
 
 	cmd.runGitCommand("set git username", "config", "user.name", cmd.gitUsername)
