fffcd1a3d3404043a1cc5202d4f72027f2b7ba69
Paul Lorenz <paul.lorenz@netfoundry.io>
2022-02-22 14:54:40 -0500

    Only configure git if running in openziti or nf organizations

--- cmd/configure_git_cmd.go
+++ cmd/configure_git_cmd.go
@@ -40,6 +40,11 @@ type configureGitCmd struct {
 }
 
 func (cmd *configureGitCmd) Execute() {
+	if val, found := os.LookupEnv("GITHUB_REPOSITORY_OWNER"); found && (val != "openziti" && val != "netfoundry") {
+		cmd.Warnf("Running in non-openziti context. Not attempting to configure git.")
+		return
+	}
+	cmd.Infof("running in openziti context, configuring git")
 	if val, found := os.LookupEnv(cmd.sshKeyEnv); found && val != "" {
 		sshKey, err := base64.StdEncoding.DecodeString(val)
 		if err != nil {
