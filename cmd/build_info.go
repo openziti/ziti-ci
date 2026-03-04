afde0f60c414e3355cf7525d9fd93b5ffa14ff99
ziti-ci <ziti-ci@netfoundry.io>
2026-03-04 15:58:19 +0000

    Release v0.5.133

--- cmd/build_info.go
+++ cmd/build_info.go
@@ -1,5 +1,5 @@
 /*
- * Copyright NetFoundry, Inc.
+ * Copyright NetFoundry Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,9 +20,9 @@
 package cmd
 
 const (
-	Version   = "v0.3.11"
-	Revision  = "169c17d6f16f"
-	Branch    = "v1"
-	BuildUser = "Paul Lorenz"
-	BuildDate = "2020-02-10 14:50:41"
+	Version   = "v0.5.133"
+	Revision  = "6a35fde2c78e"
+	Branch    = "unknown"
+	BuildUser = "unknown"
+	BuildDate = "2026-03-04T15:58:19Z"
 )
